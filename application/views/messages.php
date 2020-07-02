b832131d7cc104ba01cb6343741d5c94ca9c7327
jarleysaldarriaga <jesaldarriaga33@gmail.com>
2020-07-02 16:44:39 -0500

    create view message basic

--- application/views/messages.php
+++ application/views/messages.php
@@ -3,12 +3,30 @@
 
         <div class="row">
             <div class="col-md-6 col-md-offset-3">
-                <form action="" method="post">
-                    <select name="person" id="">
+                <form action="" method="post" class="mt-4">
+                    <div class="form-group">
+                        <label for="" class="col-sm-2 col-sm-2 control-label">Destinatario</label>
+                    </div>
+                    <div class="form-group col-sm-10">
+                        <select name="person" id="">
+                            <option class="form-control" value="0" disabled>Selecciona un usuario:</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="" class="col-sm-2 col-sm-2 control-label">Mensaje:</label>
+                        <div class="col-sm-10">
+                            <textarea class="form-control" name="text" id="" cols="20" rows="10"></textarea>
+                        </div>
+                    </div> 
+                    <div class="form-group mt-2">
+                        <label for="" class="col-sm-2 col-sm-2 control-label"></label> <br>
+                        <div class="col-sm-10">
+                            <input class="form-control" type="submit" value="Enviar">
+                        </div>
+                    </div>
                     
-                    </select>
-                    <textarea name="text" id="" cols="30" rows="10"></textarea>
-                    <input type="submit" value="Enviar">
+                    
+
                 </form>
             </div>
         </div>
